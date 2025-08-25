f3a33b39e5d0a72b3170ba582e36829aa419c104
CataphractScript <dowlatabadiparsa@gmail.com>
2025-08-25 23:54:41 +0330

    feat(data): add scenarios.cpp for test cases

---
+++ data/scenarios.cpp
@@ -0,0 +1,23 @@
+#include "../include/sim/simulator.hpp"
+#include "../include/domain/city.hpp"
+#include "../include/domain/missile.hpp"
+#include "../include/graph/graph.hpp"
+#include "../include/data/missile_data.hpp"
+
+int main() {
+    int choose_scenario;
+    std::cin >> choose_scenario;
+
+    switch (choose_scenario)
+    {
+    case 1:
+
+    case 2:
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
