14cfc3733e789796d08282bea9b9564af97fad73
M-M-Soleimani <m.m.soleimani84@gmail.com>
2025-08-26 21:11:42 +0330

    feat(graph): add load_graph_for_scenario1 implementation

--- include/data/graph_data.hpp
+++ include/data/graph_data.hpp
@@ -3,5 +3,8 @@
 #include "../graph/graph.hpp"
 
 
+// Initialize Graph for Scenario 1 with all cities across: IRN, IRQ, SYR, LBN, JOR, ISR
+Graph load_graph_for_scenario1();
+
 // Initialize Graph for Scenario 2 with all cities across: IRN, IRQ, SYR, LBN, JOR, ISR
 Graph load_graph_for_scenario2();
